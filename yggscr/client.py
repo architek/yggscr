8aabe878819734c013160bb6a0e07de3b9f00a17
Laurent Kislaire <teebeenator@gmail.com>
2018-07-24 16:50:19 +0200

    Add bt client conf

---
+++ yggscr/client.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import xmlrpc.client
+import socket
+from base64 import b64encode
+from transmissionrpc import Client as tclient
+
+def rtorrent_add_torrent(rpc_url, torrent_binary=None, torrent_file=None):
+    if torrent_binary is None:
+        if torrent_file is None:
+            return 1
+        else:
+            with open(torrent_file,"rb") as fh:
+                torrent_binary = fh.read()
+    server = xmlrpc.client.ServerProxy(rpc_url)
+    try:
+        server.load.raw_start('', xmlrpc.client.Binary(torrent_binary))
+    except (socket.error, xmlrpc.client.Error) as cause:
+        print("ERROR: %s" % cause)
+        return 1
+    else:
+        return 0
+
+def transmission_add_torrent(ip, port, torrent_binary):
+    try:
+        tc = tclient(ip, port)
+        tc.add_torrent(b64encode(torrent_binary).decode('ascii'))
+        return 0
+    except:
+        return 1
+

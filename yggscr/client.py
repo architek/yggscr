76fa109f064291b360b2f43ef45f85f289086407
Laurent <root@teebee.n0n3m.com>
2018-07-26 16:20:25 +0200

    Deluge done

--- yggscr/client.py
+++ yggscr/client.py
@@ -5,38 +5,36 @@ import xmlrpc.client
 from transmissionrpc import Client as tclient
 from deluge_client import DelugeRPCClient
 
+
 def rtorrent_add_torrent(rpc_url, torrent_binary=None, torrent_file=None):
     if torrent_binary is None:
         if torrent_file is None:
             return 1
         else:
-            with open(torrent_file,"rb") as fh:
+            with open(torrent_file, "rb") as fh:
                 torrent_binary = fh.read()
     try:
         server = xmlrpc.client.ServerProxy(rpc_url)
         server.load.raw_start('', xmlrpc.client.Binary(torrent_binary))
-    except (socket.error, xmlrpc.client.Error) as cause:
-        print("ERROR: %s" % cause)
-        return 1
-    else:
-        return 0
+    except (socket.error, xmlrpc.client.Error) as e:
+        print("Rtorrent Add torrent exception %s" % e)
+
 
 def transmission_add_torrent(ip, port, torrent_binary):
     try:
         tc = tclient(ip, port)
         tc.add_torrent(b64encode(torrent_binary).decode('ascii'))
-        return 0
-    except:
-        return 1
+    except Exception as e:
+        print("Transmission Add torrent exception %s" % e)
+
 
 def deluge_add_torrent(ip, port, user, password, torrent_binary):
     try:
-        client = DelugeRPCClient(ip, port, user, password, automatic_reconnect=False)
+        client = DelugeRPCClient(ip, int(port), user, password)
         client.connect()
-        client.core.add_torrent_file(
-            filedump=b64encode(torrent_binary).decode('ascii'))
-        return 0
-    except:
-        return 1
-
-
+        return client.core.add_torrent_file(
+            filename="",
+            filedump=b64encode(torrent_binary).decode('ascii'),
+            options={})
+    except Exception as e:
+        print("Deluge Add torrent exception %s" % e)

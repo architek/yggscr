50e5c7af7e577eb631e0f0855ecf0d9ecb7796c1
Laurent Kislaire <teebeenator@gmail.com>
2018-10-16 16:59:03 +0200

    Uppercase

--- docker/stage/conf-setup.sh
+++ docker/stage/conf-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cp /opt/yggscr/conf/* .
 sed 's/^;*\s*host\s*=\s*127.0.0.1/host = 0.0.0.0/' -i yserver.cfg
-addgroup -g $def_guid host_group
+addgroup -g $DEF_GUID host_group
 adduser uwsgi host_group
 
 exec "$@"

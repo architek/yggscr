f8635855f4a7fd85b7aff9f0f3cd72efc7bf13bc
Laurent Kislaire <teebeenator@gmail.com>
2018-10-20 14:22:29 +0200

    Detect final host for keeping headers and cookies

--- src/yggscr/const.py
+++ src/yggscr/const.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
+import requests
 
-# Normally not needed because of http redirect but breaks with SSL FIXME
-YGG_HOST = "www3.yggtorrent.to"
-
+YGG_HOST = "www.yggtorrent.to"
 YGG_HOME = "https://" + YGG_HOST
 
-DL_TPL = YGG_HOME           + "/engine/download_torrent?id={id}"
-TORRENT_URL = YGG_HOME      + "/torrents/"
-SEARCH_URL = YGG_HOME       + "/engine/search"
-TOP_DAY_URL = YGG_HOME      + "/engine/ajax_top_query/day"
-TOP_WEEK_URL = YGG_HOME     + "/engine/ajax_top_query/week"
-TOP_MONTH_URL = YGG_HOME    + "/engine/ajax_top_query/month"
-EXCLUS_URL = YGG_HOME       + "/torrents/exclus"
-TOP_SEED_URL = YGG_HOME     + "/engine/mostseeded"
-RSS_TPL = YGG_HOME          + "/rss?type=1&parent_category={category}"
-SHOUT_URL = YGG_HOME        + "/forum/index.php?shoutbox/refresh"
+
+def detect_redir():
+    global YGG_HOME, DL_TPL, TORRENT_URL, SEARCH_URL, TOP_DAY_URL, TOP_WEEK_URL, TOP_MONTH_URL, \
+           EXCLUS_URL, TOP_SEED_URL, RSS_TPL, SHOUT_URL
+    ir = requests.get(YGG_HOME, allow_redirects=False)
+    if ir.status_code in [307, 301]:
+        YGG_HOME = ir.headers['Location']
+    DL_TPL = YGG_HOME           + "engine/download_torrent?id={id}"
+    TORRENT_URL = YGG_HOME      + "torrents/"
+    SEARCH_URL = YGG_HOME       + "engine/search"
+    TOP_DAY_URL = YGG_HOME      + "engine/ajax_top_query/day"
+    TOP_WEEK_URL = YGG_HOME     + "engine/ajax_top_query/week"
+    TOP_MONTH_URL = YGG_HOME    + "engine/ajax_top_query/month"
+    EXCLUS_URL = YGG_HOME       + "torrents/exclus"
+    TOP_SEED_URL = YGG_HOME     + "engine/mostseeded"
+    RSS_TPL = YGG_HOME          + "rss?type=1&parent_category={category}"
+    SHOUT_URL = YGG_HOME        + "forum/index.php?shoutbox/refresh"
 
 
 def get_dl_link(id):
     return DL_TPL.format(id=id)
+
+
+detect_redir()

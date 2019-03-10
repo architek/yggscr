240f36af795d9638f36973e87abf46558c0e02e7
Laurent Kislaire <teebeenator@gmail.com>
2019-03-10 23:20:06 +0100

    Gitlab/CI: Up

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -8,23 +8,10 @@
 import src.yggscr
 import src.yggscr.shell
 import src.yserver.core
-import src.yggscr.__main__
-
-
-def test_import_yggscr():
-    """Test imports."""
-    src.yggscr
-    src.yggscr.__main__.main()
-
-
-def test_import_yserver():
-    """Test imports."""
-
-    src.yserver
 
 
 def test_import_yshell():
     """Test imports."""
 
-    s = src.yggscr.shell
-    # assert isinstance(s, src.yggscr.shell.YggShell)
+    s = src.yggscr.shell.YggShell()
+    assert isinstance(s, src.yggscr.shell.YggShell)

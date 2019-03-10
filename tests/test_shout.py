c963db70788d6556ee6b6b961aa7dcde05f43646
Laurent Kislaire <teebeenator@gmail.com>
2019-03-11 00:40:18 +0100

    CI / Pytest

--- tests/test_shout.py
+++ tests/test_shout.py
@@ -43,16 +43,6 @@ def test_shout_diff():
     assert success
 
 
-# 20s test
-@pytest.mark.timeout(20)
-def test_shout_main():
-    print("Testing for 20s")
-    try:
-        main_shout([])
-    except:
-        pass
-
-
 @pytest.mark.skipif(sys.version_info[:2] == (3, 5), reason="python3.5 doesn't have tmp_path")
 def test_shout2(tmp_path):
     f = tmp_path / "foo.txt"
